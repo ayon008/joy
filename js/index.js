ba7f5447f8cac4f891406ea5ce9cac06dcf94c5c
ayon008 <shariar.ayon128@gmail.com>
2023-06-18 18:55:39 +0600

    final

--- js/index.js
+++ js/index.js
@@ -2,28 +2,26 @@ document.getElementById('id1').addEventListener('mouseenter', function () {
     const color = '#515151';
     document.getElementById('id1').style.background = color;
     document.getElementById('another').style.background = color;
-    document.getElementById('dim').style.opacity = 0.1;
-    document.getElementById('another-dim').style.opacity = 0.1;
+    document.getElementById('dim').style.filter = "brightness(" + 0.3 + ")";
+    document.getElementById('another-dim').style.filter = "brightness(" + 0.3 + ")";
 })
 document.getElementById('id1').addEventListener('mouseleave', function () {
-    const color = '#515151';
     document.getElementById('id1').style.background = '';
     document.getElementById('another').style.background = '';
-    document.getElementById('dim').style.opacity = '';
-    document.getElementById('another-dim').style.opacity = '';
+    document.getElementById('dim').style.filter = '';
+    document.getElementById('another-dim').style.filter = '';
 })
 
 document.getElementById('another').addEventListener('mouseenter', function () {
     const color = '#515151';
     document.getElementById('id1').style.background = color;
     document.getElementById('another').style.background = color;
-    document.getElementById('dim').style.opacity = 0.1;
-    document.getElementById('another-dim').style.opacity = 0.1;
+    document.getElementById('dim').style.filter = "brightness(" + 0.3 + ")";
+    document.getElementById('another-dim').style.filter = "brightness(" + 0.3 + ")";
 })
 document.getElementById('another').addEventListener('mouseleave', function () {
-    const color = '#515151';
     document.getElementById('id1').style.background = '';
     document.getElementById('another').style.background = '';
-    document.getElementById('dim').style.opacity = '';
-    document.getElementById('another-dim').style.opacity = '';
+    document.getElementById('dim').style.filter = '';
+    document.getElementById('another-dim').style.filter = '';
 })
